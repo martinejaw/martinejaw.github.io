3c51d972d5290046c36c14551277384e5b3d0c9e
martinejaw <martinezequieljaworski@gmail.com>
2020-07-31 18:01:04 -0300

    Init commit

---
+++ precache-manifest.fb0c3656d119f416b21b6a582ec37399.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c8e9309870f6d71206bc9e4e27d8d4e",
+    "url": "/optimizer-pl-io/index.html"
+  },
+  {
+    "revision": "d111d6aea23854e8acde",
+    "url": "/optimizer-pl-io/static/css/2.07228003.chunk.css"
+  },
+  {
+    "revision": "b8ba523d3b6fafd82ca4",
+    "url": "/optimizer-pl-io/static/css/main.ddfd5615.chunk.css"
+  },
+  {
+    "revision": "d111d6aea23854e8acde",
+    "url": "/optimizer-pl-io/static/js/2.7ebe5311.chunk.js"
+  },
+  {
+    "revision": "66e6ba2872d260c3574d41589d7278d6",
+    "url": "/optimizer-pl-io/static/js/2.7ebe5311.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8ba523d3b6fafd82ca4",
+    "url": "/optimizer-pl-io/static/js/main.cd62044e.chunk.js"
+  },
+  {
+    "revision": "feac02a915d41815534f",
+    "url": "/optimizer-pl-io/static/js/runtime-main.4a332c2c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/optimizer-pl-io/static/media/logo.5d5d9eef.svg"
+  }
+]);
