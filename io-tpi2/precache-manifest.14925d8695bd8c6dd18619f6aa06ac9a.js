342edf4f827204533660b0c99e9ed777e453a8f4
martinejaw <martinezequieljaworski@gmail.com>
2020-08-07 14:23:24 -0300

    Probando logo

---
+++ io-tpi2/precache-manifest.14925d8695bd8c6dd18619f6aa06ac9a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68b5096c81d042d71a9509071475a1c2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2b2fd60f31668df90e01",
+    "url": "./static/css/2.07228003.chunk.css"
+  },
+  {
+    "revision": "c2ae4ab2da13b7564747",
+    "url": "./static/css/main.ddfd5615.chunk.css"
+  },
+  {
+    "revision": "2b2fd60f31668df90e01",
+    "url": "./static/js/2.35857f7b.chunk.js"
+  },
+  {
+    "revision": "66e6ba2872d260c3574d41589d7278d6",
+    "url": "./static/js/2.35857f7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2ae4ab2da13b7564747",
+    "url": "./static/js/main.06164045.chunk.js"
+  },
+  {
+    "revision": "0d87a42795fa5bc9ab28",
+    "url": "./static/js/runtime-main.f843f7a9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
