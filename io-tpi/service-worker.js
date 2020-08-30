61cf784be920a974bb4de90d28616ba45fcebe38
martinejaw <martinezequieljaworski@gmail.com>
2020-08-30 14:07:14 -0300

    Arreglo modelo 5

--- io-tpi/service-worker.js
+++ io-tpi/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/io-tpi/precache-manifest.8cbb8a0bdb7aba55c6a76a55119ad96d.js"
+  "./precache-manifest.7b24a863f7fcc66150b1c18a7afe28b4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/io-tpi/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
